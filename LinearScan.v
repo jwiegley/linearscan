8232b67e3ddc2f90f59afccc0b0f1001b2ef7c4f
John Wiegley <johnw@newartisans.com>
2014-09-15 19:25:13 +0100

    Add proof of fin_list, thanks to Daniel Schepler

--- LinearScan.v
+++ LinearScan.v
@@ -31,6 +31,18 @@ Generalizable All Variables.
 
 (** The following are extensions to the Coq standard library. *)
 
+(** ** option *)
+
+Fixpoint catMaybes {a : Set} (l : list (option a)) : list a :=
+  match l with
+    | nil => nil
+    | cons None xs => catMaybes xs
+    | cons (Some x) xs => x :: catMaybes xs
+  end.
+
+Definition mapMaybe {a b : Set} (f : a -> option b) (l : list a) : list b :=
+  catMaybes (map f l).
+
 (** ** Lists *)
 
 Section Elems.
@@ -52,12 +64,6 @@ Function NoDup_from_list (l : list a) {measure length l} : option (NoDup l) :=
       end
   end.
 Proof. auto. Qed.
-
-Lemma NoDup_uncons : forall (x : a) xs, NoDup (x :: xs) -> NoDup xs.
-Proof.
-  intros. inversion H; subst.
-  assumption.
-Qed.
 
 Lemma NoDup_swap : forall (xs ys : list a), NoDup (xs ++ ys) -> NoDup (ys ++ xs).
 Proof.
@@ -81,7 +87,7 @@ Proof.
     apply in_app_iff.
     apply in_app_iff in H0.
     intuition.
-  apply NoDup_uncons in H.
+  inversion H.
   apply NoDup_swap.
   assumption.
 Defined.
@@ -97,7 +103,7 @@ Proof.
   induction xs. auto.
   apply IHxs.
   rewrite <- app_comm_cons in H.
-  apply NoDup_uncons in H.
+  inversion H.
   assumption.
 Defined.
 
@@ -170,7 +176,7 @@ Proof.
   - constructor.
   - apply (Permutation_not_in x) in H.
       apply NoDup_cons. assumption.
-      apply NoDup_uncons in H0.
+      inversion H0.
       apply IHPermutation.
       assumption.
     inversion H0. assumption.
@@ -186,6 +192,26 @@ Proof.
     apply not_in_uncons in H2.
     apply NoDup_cons; assumption.
   - auto.
+Qed.
+
+Lemma NoDup_map : forall {a b : Set} (l : list a)
+  (f : a -> b) (f_inj : forall x y, f x = f y -> x = y),
+  NoDup l -> NoDup (map f l).
+Proof.
+  intros. induction l; simpl. constructor.
+  inversion H; subst.
+  apply IHl in H3.
+  inversion H3.
+    constructor. auto.
+    constructor.
+  apply NoDup_cons.
+    rewrite H0.
+    unfold not in *. intros H5.
+    apply in_map_iff in H5.
+    destruct H5. destruct H5.
+    apply f_inj in H5. subst.
+    contradiction.
+  rewrite H0. assumption.
 Qed.
 
 (*
@@ -375,8 +401,8 @@ Proof. induction n; [ apply F1 | apply FS; apply IHn ]. Defined.
 Definition ultimate_from_nat (n : nat) (H : n > 0) : fin n.
 Proof. induction n; [ omega | apply ultimate_Sn ]. Defined.
 
-(** Given a value [x] of type [fin n], possibly return the next lower
-    inhabitant of type [y], such that y < x. *)
+(** Given a value [x] of type [fin n], return the next lower inhabitant [y],
+    such that y < x.  Returns [None] if [x = 0]. *)
 Definition pred_fin {n} (f : fin n) : option (fin n).
   apply to_nat in f.
   destruct f.
@@ -385,6 +411,22 @@ Definition pred_fin {n} (f : fin n) : option (fin n).
   apply Le.le_Sn_le in l.
   apply (from_nat x l).
 Defined.
+
+(** Given a value of type [fin (S n)], return the equivalent [fin n],
+    returning None if the input value was the highest possible value of [fin
+    (S n)]. *)
+
+Definition fin_reduce {n : nat} (x : fin (S n)) : option (fin n) :=
+  let n' := fin_to_nat x in
+  match le_lt_dec n n' with
+  | right H => Some (from_nat n' H)
+  | left _  => None
+  end.
+
+(* Lemma fin_reduce_inj : forall n (x y : fin (S n)), *)
+(*   fin_reduce x = fin_reduce y -> x = y. *)
+(* Proof. *)
+(*   intros. *)
 
 (** [to_nat] and [from_nat] compose to an identity module the hypothesis that
     [n < m]. *)
@@ -457,6 +499,75 @@ Proof.
     apply eq_add_S in H.
     subst. reflexivity.
   - f_equal. f_equal. assumption.
+Qed.
+
+Definition fin_Sn_inv {n:nat} (P : fin (S n) -> Type)
+  (PO : P F1) (PS : forall y : fin n, P (FS y)) :
+  forall x : fin (S n), P x :=
+  fun x =>
+    match x in (t Sn) return
+      (match Sn return (fin Sn -> Type) with
+       | 0 => fun _ => unit
+       | S n' => fun x => forall (P : fin (S n') -> Type),
+         P F1 -> (forall y : fin n', P (FS y)) ->
+         P x
+       end x) with
+    | F1 _ => fun P PO PS => PO
+    | FS _ y => fun P PO PS => PS y
+    end P PO PS.
+
+Definition FS_inv {n} (x : fin (S n)) : option (fin n) :=
+  fin_Sn_inv (fun _ => option (fin n)) None (@Some _) x.
+
+Definition map_FS_inv {n:nat} (l : list (fin (S n))) : list (fin n) :=
+  catMaybes (map FS_inv l).
+
+Lemma map_FS_inv_len_noO : forall {n} (l : list (fin (S n))),
+  ~ In F1 l -> length (map_FS_inv l) = length l.
+Proof.
+  induction l; simpl. reflexivity.
+  destruct a using fin_Sn_inv; simpl; intuition.
+Qed.
+
+Lemma map_FS_inv_len_NoDup : forall {n} (l : list (fin (S n))),
+  NoDup l -> length l <= S (length (map_FS_inv l)).
+Proof.
+  induction 1; simpl.
+    apply le_0_n.
+  unfold map_FS_inv, catMaybes. simpl.
+  destruct x using fin_Sn_inv; simpl; intros.
+    fold (@catMaybes).
+    pose (map_FS_inv_len_noO l H).
+    unfold map_FS_inv in *.
+    rewrite e. reflexivity.
+  auto with arith.
+Qed.
+
+Lemma in_map_FS_inv : forall {n} (l : list (fin (S n))) (y : fin n),
+  In y (map_FS_inv l) -> In (FS y) l.
+Proof.
+  induction l; simpl. trivial.
+  destruct a using fin_Sn_inv; simpl. auto.
+  destruct 1. left; f_equal; trivial.
+  right; auto.
+Qed.
+
+Lemma map_FS_inv_NoDup : forall {n:nat} (l : list (fin (S n))),
+  NoDup l -> NoDup (map_FS_inv l).
+Proof.
+  induction 1; simpl. constructor.
+  destruct x using fin_Sn_inv; simpl. trivial.
+  constructor; trivial.
+  contradict H.
+  apply in_map_FS_inv; trivial.
+Qed.
+
+Theorem fin_list n (l : list (fin n)) : NoDup l -> length l <= n.
+Proof.
+  induction n as [|n']; intros.
+    destruct l as [|hd ?]; trivial; inversion hd.
+  apply le_trans with (1 := map_FS_inv_len_NoDup l H).
+  auto using le_n_S, map_FS_inv_NoDup.
 Qed.
 
 (** *** Comparison of values from the same finite set. *)
@@ -737,15 +848,17 @@ Proof.
   destruct unhandled0.
     apply None.
   apply Some.
-  exists {| unhandled        := unhandled0
-          ; active           := active0
-          ; inactive         := inactive0
-          ; handled          := handled0
-          ; getInterval      := getInterval0
-          ; assignments      := assignments0
-          ; lists_are_unique := NoDup_uncons _ _ _ lists_are_unique0
-          |}.
+  eexists {| unhandled        := unhandled0
+           ; active           := active0
+           ; inactive         := inactive0
+           ; handled          := handled0
+           ; getInterval      := getInterval0
+           ; assignments      := assignments0
+           |}.
   apply i.
+  Grab Existential Variables.
+  inversion lists_are_unique0.
+  assumption.
 Defined.
 
 (* jww (2014-09-14): I would like for this function to only be callable if
@@ -992,13 +1105,6 @@ Fixpoint checkActiveIntervals st pos : ScanState :=
         go st st1 xs pos
     end in
   go st st (active st) pos.
-
-Theorem fin_list : forall n (l : list (fin n)), NoDup l -> length l <= n.
-Proof.
-  intros.
-  induction H; simpl. omega.
-  
-Admitted.
 
 Lemma ScanState_active_bounded : forall st, length (active st) <= nextInterval st.
 Proof.
