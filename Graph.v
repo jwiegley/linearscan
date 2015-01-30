9775838e19a33f826c506bfc257241e221c84b66
John Wiegley <johnw@newartisans.com>
2015-01-30 13:12:19 -0500

    Get the Graph.v code working

--- Graph.v
+++ Graph.v
@@ -47,62 +47,63 @@ Fixpoint tsort' fuel l roots g :=
   (* The fuel represents the fact that we must only call tsort' once for
      each vertex in the graph. *)
   if fuel isn't S fuel then rev l else
-  if edges g isn't (se, de) :: es then rev l ++ roots else
+  if edges g isn't (se, de) :: es then rev l else
 
   let: next :=
     if roots is n :: s
     then (n :: s, g)
     else ([:: de], addEdge (se, None) $
-                   addEdge (None, de) $
                    removeEdge (se, de) $ g) in
   if next isn't (n :: s, g') then [::] else
 
-  let outEdges := outbound n g' in
-  let g'' := foldr removeEdge g' outEdges in
-  let outNodes := map (@snd _ _) outEdges in
-  let s' := s ++ filter (@nilp _ \o inbound ^~ g'') outNodes in
-  tsort' fuel (n :: l) s' g''.
+  let outEdges    := outbound n g' in
+  let: (res, g'') := foldl (fun acc e =>
+                              let: (res, g'') := acc in
+                              (e :: res, removeEdge e g''))
+                           ([::], g') outEdges in
+  let outNodes    := map (@snd _ _) outEdges in
+  let s'          := s ++ filter (@nilp _ \o inbound ^~ g'') outNodes in
+
+  tsort' fuel (l ++ res) s' g''.
 
 Definition topsort g :=
   let noInbound :=
       (fun xs => [seq x <- vertices g | x \notin xs])
       (map (@snd _ _) (edges g)) in
-  tsort' (size (vertices g)) [::] noInbound g.
+  (* +1 is added to the fuel in case None is injected as a root *)
+  tsort' (size (vertices g)).+1 [::] noInbound g.
 
 End Topsort.
 
 Arguments emptyGraph [a].
+Arguments addVertex [a] v g.
+Arguments addEdge [a] e g.
 Arguments removeEdge [a] _ g.
 Arguments connections [a] _ _ g.
 Arguments outbound [a] _ g.
 Arguments inbound [a] _ g.
 Arguments topsort [a] g.
-Arguments addVertex [a] v g.
-Arguments addEdge [a] e g.
 
-Compute
-  ( addEdge (Some  1, Some  3)
-  $ addEdge (Some  4, Some  5)
-  $ addEdge (Some  9, Some  7)
-  $ addEdge (Some 10, Some 11)
-  $ addEdge (Some 11, Some 10)
-  $ addEdge (Some  7, Some  1)
-  $ addEdge (Some  6, Some  2)
-  $ addEdge (Some  2, Some  4)
-  $ addEdge (Some  5, Some  6)
-  $ emptyGraph).
+(* Compute *)
+(*   ( addEdge (Some  1, Some  3) *)
+(*   $ addEdge (Some  4, Some  5) *)
+(*   $ addEdge (Some  9, Some  7) *)
+(*   $ addEdge (Some 10, Some 11) *)
+(*   $ addEdge (Some 11, Some 10) *)
+(*   $ addEdge (Some  7, Some  1) *)
+(*   $ addEdge (Some  6, Some  2) *)
+(*   $ addEdge (Some  2, Some  4) *)
+(*   $ addEdge (Some  5, Some  6) *)
+(*   $ emptyGraph). *)
 
-Compute topsort
-  ( addEdge (Some  1, Some  3)
-  $ addEdge (Some  4, Some  5)
-  $ addEdge (Some  9, Some  7)
-  $ addEdge (Some 10, Some 11)
-  $ addEdge (Some 11, Some 10)
-  $ addEdge (Some  7, Some  1)
-  $ addEdge (Some  6, Some  2)
-  $ addEdge (Some  2, Some  4)
-  $ addEdge (Some  5, Some  6)
-  $ emptyGraph).
-
-Extraction Language Haskell.
-Recursive Extraction topsort.
+(* Compute topsort *)
+(*   ( addEdge (Some  1, Some  3) *)
+(*   $ addEdge (Some  4, Some  5) *)
+(*   $ addEdge (Some  9, Some  7) *)
+(*   $ addEdge (Some 10, Some 11) *)
+(*   $ addEdge (Some 11, Some 10) *)
+(*   $ addEdge (Some  7, Some  1) *)
+(*   $ addEdge (Some  6, Some  2) *)
+(*   $ addEdge (Some  2, Some  4) *)
+(*   $ addEdge (Some  5, Some  6) *)
+(*   $ emptyGraph). *)
