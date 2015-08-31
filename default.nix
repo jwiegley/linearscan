ce2ea2559975a92f334b162637bcd7d2977b9edf
John Wiegley <johnw@newartisans.com>
2015-08-31 10:28:38 -0700

    Update version to 0.10.1

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, containers, mtl, stdenv, transformers }:
 mkDerivation {
   pname = "linearscan";
-  version = "0.10.0";
+  version = "0.10.1";
   src = ./.;
   buildDepends = [ base containers mtl transformers ];
   homepage = "http://github.com/jwiegley/linearscan";
