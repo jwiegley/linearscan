496112da148c3699db87b079bdfc453d8c3d2b4a
John Wiegley <johnw@newartisans.com>
2015-09-01 09:18:09 -0700

    Add a build script for more quickly building linearscan

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -ex
+
+cd ~/src/linearscan
+
+(cd Hask; load-env-coq85 make)
+
+# Avoid timestamps updates for files whose contents are unchanged
+rsync -a --delete LinearScan/ LinearScan.base/
+load-env-coq85 make
+mv LinearScan LinearScan.new
+mv LinearScan.base LinearScan
+rsync -cr --delete LinearScan.new/ LinearScan/
+rm -fr LinearScan.new
+
+xargs -0 <<EOF nix-cabal-shell --pure --command
+       ghc --make Setup                         \
+    && ./Setup configure --enable-tests         \
+    && ./Setup build -j test                    \
+    && ./dist/build/test/test $@
+EOF
